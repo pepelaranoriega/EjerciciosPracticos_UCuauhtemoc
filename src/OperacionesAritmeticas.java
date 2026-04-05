8f9e2b91e2bd9b2e8690f59b3010393b103a8879
Pepe Lara <pepelaranoriega@icloud.com>
2026-04-04 20:29:34 -0700

    Update OperacionesAritmeticas.java

--- src/OperacionesAritmeticas.java
+++ src/OperacionesAritmeticas.java
@@ -7,8 +7,10 @@ public class OperacionesAritmeticas {
         float numero1;
         float numero2;
         float suma;
+        float resta;
         float multiplicacion;
-
+        float division;
+        float restoDeDivision;
         Scanner leer = new Scanner(System.in);
 
         while (true) { //Validamos que el primer número sea ingresado correctamente.
@@ -21,7 +23,6 @@ public class OperacionesAritmeticas {
                 leer.next();
             }
         }
-
         while (true) { //Validamos que el segundo número se ingresado correctamente.
             try {
                 System.out.println("Ingresa el segundo valor: ");
@@ -35,7 +36,21 @@ public class OperacionesAritmeticas {
 
         suma = numero1 + numero2;
         multiplicacion = numero1 * numero2;
+        resta = numero1 - numero2;
+
         System.out.println("La suma de los números nos da como resultado: " + suma);
+        System.out.println("La resta de los números nos da como resultado: " + resta);
         System.out.println("La multiplicación de los números nos da como resultado: " + multiplicacion);
+        if (numero2 == 0) { /* Si el segundo número es igual a cero, nos arrojará el siguiente
+                               mensaje y terminará el programa*/
+            System.out.println("La division no puede ser entre cero");
+            return;
+        } else {
+            division = numero1 / numero2;
+            restoDeDivision = numero1 % numero2;
+            System.out.println("La división de los números nos da como resultado: " + division);
+            System.out.println("El restante de la división es: " + restoDeDivision);
+        }
     }
 }
+
